210bdf6c7e3456d1b1f1772c7893bcf0a78d4542
Sandeep Shrestha <sandeepstha64@gmail.com>
2025-03-05 17:28:08 +0545

    [in progress] working on local storage

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TodoLists from "../components/TodoLists";
 import CompletedLists from "../components/CompletedLists";
 
 import '../css/home.css'
 
 export default function Home() {
+
+    const [input, setInput] = useState();
+
+    const onChangeHandler = (e) => {
+        setInput(e.target.value);
+    }
+
+    // to store value in localstorage
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        localStorage.setItem("task", input);
+    }
+
+
     return (
         <div className="todo-wrapper">
-            <form className="form form-vertical d-flex">
-                <input className="form-vertical__input" type="text" name="name" placeholder="Eg: I have to do my homework" />
+            <form className="form form-vertical d-flex" onSubmit={onSubmitHandler}>
+                <input className="form-vertical__input" type="text" name="task" onChange={onChangeHandler} required={true} placeholder="Eg: I have to do my homework" />
                 <button className="form-vertical__button" type="submit">
                     <i className="bi bi-plus-lg"></i>
                 </button>
